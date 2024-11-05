2cb781e1b695f7aca39a1011edb0c3ba6c725c69
macrwx <zwmaciek04@gmail.com>
2024-11-05 20:54:08 +0100

    Add car engine kinds

--- Source/CarEngine.h
+++ Source/CarEngine.h
@@ -1,9 +1,21 @@
 #ifndef CAR_ENGINE_H
 #define CAR_ENGINE_H
 
+enum class CarEngineKind : U8 {
+    /** Combustion engine using petrol as fuel */
+    Petrol,
+    /** Combustion engine using diesel as fuel */
+    Diesel,
+    /** Electric motor instead of combustion engine */
+    Electric,
+    /** Combustion engine and electric motor */
+    Hybrid,
+};
+
 class CarEngine {
 private:
     U8 const *_name;
+    CarEngineKind _kind;
     F32 _power, _torque;
     F32 _price;
 };
