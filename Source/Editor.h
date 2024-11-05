c0ff1d5009ad842d100a5a3b05ee79984604981e
macrwx <zwmaciek04@gmail.com>
2024-11-05 20:48:34 +0100

    Add entry point

---
+++ Source/Editor.h
@@ -0,0 +1,14 @@
+#ifndef EDITOR_H
+#define EDITOR_H
+
+struct Editor {
+    static CarConfig *savedConfigs;
+
+    static S32 Main();
+};
+
+S32 Editor::Main() {
+    return 0;
+}
+
+#endif //EDITOR_H
---
+++ Source/main.cc
@@ -0,0 +1,11 @@
+#include "Base/Types/BaseTypes.h"
+
+#include "CarEngine.h"
+#include "CarModel.h"
+#include "CarConfig.h"
+
+#include "Editor.h"
+
+S32 main(S32 argumentCount, char *arguments[]) {
+    return Editor::Main();
+}
